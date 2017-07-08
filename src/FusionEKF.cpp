71368bbb3db20883057209acb71f3994147086f6
Ruslan Aminev <amirusrif@gmail.com>
2017-07-08 17:50:28 +0300

    First measurement, state vector initialization

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -36,8 +36,10 @@ FusionEKF::FusionEKF() {
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
   */
+    H_laser_ << 1.0, 0, 0, 0,
+                0, 1.0, 0, 0;
 
-
+    Hj_ << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
 }
 
 /**
@@ -61,17 +63,27 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     // first measurement
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1;
+    ekf_.x_ << 0, 0, 0, 0;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
+      ro, theta,ro_dot
       */
+      float ro = measurement_pack.raw_measurements_(0);
+      float theta = measurement_pack.raw_measurements_(1);
+      float dro = measurement_pack.raw_measurements_(2);
+      ekf_.x_(0) = ro * cos(theta);
+      ekf_.x_(1) = ro * sin(theta);
+      ekf_.x_(2) = dro * cos(theta);
+      ekf_.x_(3) = dro * sin(theta);
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
       */
+      ekf_.x_(0) = measurement_pack.raw_measurements_(0);
+      ekf_.x_(1) = measurement_pack.raw_measurements_(1);
     }
 
     // done initializing, no need to predict or update
@@ -104,7 +116,9 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    */
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+      Hj_ << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
     // Radar updates
+      //if()
   } else {
     // Laser updates
   }
